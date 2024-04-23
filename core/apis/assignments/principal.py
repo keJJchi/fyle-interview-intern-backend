5fdd725b123fd0c7d898e6d129eb4208f22152de
Kirill <kejjchii@gmail.com>
2024-04-23 21:46:42 +0400

    'Returns all list of assignments' and 'Re-grade an assignment'

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -2,7 +2,8 @@ from flask import Blueprint
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
+from core.models.assignments import Assignment, AssignmentStateEnum
+from core.libs.assertions import assert_valid
 
 from .schema import AssignmentSchema, AssignmentGradeSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
@@ -10,24 +11,28 @@ principal_assignments_resources = Blueprint('principal_assignments_resources', _
 
 @principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
-def list_assignments(p):
+def list_assignments(_):
     """Returns all list of assignments"""
-    principal_assignments = Assignment.get_assignments()
+    principal_assignments = Assignment.get_assignments_by_principal()
     principal_assignments_dump = AssignmentSchema().dump(principal_assignments, many=True)
     return APIResponse.respond(data=principal_assignments_dump)
 
 @principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
-def grade_assignment(p, incoming_payload):
-    """Grade an assignment"""
-    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+def regrade_assignment(p, incoming_payload):
+    """Re-grade an assignment"""
+    regrade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+    assignment = Assignment.get_by_id(regrade_assignment_payload.id)
 
-    graded_assignment = Assignment.mark_grade(
-        _id=grade_assignment_payload.id,
-        grade=grade_assignment_payload.grade,
-        auth_principal=p
-    )
-    db.session.commit()
-    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
-    return APIResponse.respond(data=graded_assignment_dump)
+    if getattr(assignment,"state", None) in (AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED):
+        regraded_assignment = Assignment.mark_grade(
+            _id=regrade_assignment_payload.id,
+            grade=regrade_assignment_payload.grade,
+            auth_principal=p
+        )
+        db.session.commit()
+        regraded_assignment_dump = AssignmentSchema().dump(regraded_assignment)
+        return APIResponse.respond(data=regraded_assignment_dump)
+    msg = "Assignment is in Draft state"
+    assert_valid(False, msg)
